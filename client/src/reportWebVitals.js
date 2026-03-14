00aff705fda088d640e37d604b406956f09f938a
cahcet-mca <cahcetmcasymp@gmail.com>
2026-03-14 23:40:47 +0530

    Delete client directory

--- client/src/reportWebVitals.js
+++
@@ -1,7 +0,0 @@
-// This file is optional. You can delete it or keep this empty version
-const reportWebVitals = (onPerfEntry) => {
-  // Web Vitals tracking is disabled
-  console.log('Web Vitals tracking is disabled');
-};
-
-export default reportWebVitals;
