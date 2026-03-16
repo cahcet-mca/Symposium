daeeb001220fe87ef9ee901a7f29e8fec683b2e7
cahcet-mca <cahcetmcasymp@gmail.com>
2026-03-16 23:25:08 +0530

    Delete server directory

--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyPayment, getMyRegistrations } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/verify', protect, verifyPayment);
-router.get('/my-registrations', protect, getMyRegistrations);
-
-module.exports = router;
